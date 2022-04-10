00a56ff5ba51cca715f892acb6b1aa4d1c0045d2
Joseph Paredes Gleespen <jgleespe@emich.edu>
2022-04-10 16:17:48 -0400

    pass checking + toast

--- app/src/main/java/com/company/strengthtracker/presentation/register_screen/RegisterScreen.kt
+++ app/src/main/java/com/company/strengthtracker/presentation/register_screen/RegisterScreen.kt
@@ -1,5 +1,8 @@
 package com.company.strengthtracker.presentation.register_screen
 
+import android.content.Context
+import android.view.Gravity
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
@@ -10,6 +13,7 @@ import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -28,6 +32,7 @@ fun RegisterScreen(
 ) {
 
     val screenState by remember { viewModel.registerScreenState }
+    val context = LocalContext.current
 
     when (screenState) {
         LOADING -> {
@@ -60,33 +65,6 @@ fun RegisterScreen(
         // Present registration UI
         LAUNCH -> {
 
-<<<<<<< HEAD
-            var newUserEmail by remember { mutableStateOf("") }
-            var newUserDob by remember { mutableStateOf("") }
-            var newUserId by remember { mutableStateOf("") }
-            var newUserPassText by remember { mutableStateOf("") }
-            var passConfirmation by remember { mutableStateOf("") }
-            var passErr by remember { mutableStateOf("Create a New Account") }
-            var passwordVis by remember { mutableStateOf(false) }
-
-            Box(modifier = Modifier.fillMaxWidth(0.5f)) {
-                Image(
-                    painter = painterResource(id = R.drawable.gigachad),
-                    contentDescription = "Giga Chad"
-                )
-            }
-            Card(modifier = Modifier.fillMaxWidth(0.8f)){
-                Card(modifier = Modifier.fillMaxWidth(0.4f)) {
-                    Column(
-                        modifier = Modifier.fillMaxWidth().padding(10.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Center
-                    ) {
-                      Text(text = passErr)
-                    }
-                }
-            }
-=======
             /* COLUMNS */
             // This column fills all nested composables to the entire size of the screen and centers
             Column(
@@ -94,52 +72,19 @@ fun RegisterScreen(
                 verticalArrangement = Arrangement.SpaceEvenly,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-
-<<<<<<< Updated upstream
                 var newUserEmail by remember { mutableStateOf("") }
                 var newUserDob by remember { mutableStateOf("") }
                 var newUserId by remember { mutableStateOf("") }
                 var newUserPassText by remember { mutableStateOf("") }
                 var passConfirmation by remember { mutableStateOf("") }
                 var passwordVis by remember { mutableStateOf(false) }
->>>>>>> main
 
                 Box(modifier = Modifier.fillMaxWidth(0.5f)) {
                     Image(
                         painter = painterResource(id = R.drawable.gigachad),
                         contentDescription = "Giga Chad"
-=======
-            Card(modifier = Modifier.fillMaxWidth(0.8f)) {
-                Column(
-                    modifier = Modifier.padding(10.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                        value = newUserDob,
-                        onValueChange = { newUserDob = it },
-                        label = { Text("Date of birth") }
-                    )
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-                        value = newUserEmail,
-                        onValueChange = { newUserEmail = it
-                                        },
-                        label = { Text("Email") }
-                    )
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        value = newUserId,
-                        onValueChange = { newUserId = it
-                                        },
-                        label = { Text("Username") }
->>>>>>> Stashed changes
                     )
                 }
-
                 Card(modifier = Modifier.fillMaxWidth(0.8f)) {
                     Column(
                         modifier = Modifier.padding(10.dp),
@@ -167,41 +112,16 @@ fun RegisterScreen(
                             label = { Text("Username") }
                         )
 
-<<<<<<< HEAD
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        visualTransformation = if (passwordVis) VisualTransformation.None else PasswordVisualTransformation(),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                        value = newUserPassText,
-
-                        onValueChange = {
-                            newUserPassText = it
-                        },
-                        trailingIcon = { },
-                        label = { Text("Password") }
-                    )
-                    TextField(
-                        modifier = Modifier.fillMaxWidth(),
-                        visualTransformation = if (passwordVis) VisualTransformation.None else PasswordVisualTransformation(),
-                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                        value = passConfirmation,
-                        onValueChange = {
-                            passConfirmation = it
-                        },
-                        label = { Text("Confirm password") }
-                    )
-                    Button(
-                        modifier = Modifier.fillMaxWidth(),
-                        onClick = {
-                            if (newUserPassText.equals(passConfirmation)) {
-=======
-
                         TextField(
                             modifier = Modifier.fillMaxWidth(),
                             visualTransformation = if (passwordVis) VisualTransformation.None else PasswordVisualTransformation(),
                             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                             value = newUserPassText,
-                            onValueChange = { newUserPassText = it },
+
+                            onValueChange = {
+                                newUserPassText = it
+                            },
+                            trailingIcon = { },
                             label = { Text("Password") }
                         )
                         TextField(
@@ -209,36 +129,41 @@ fun RegisterScreen(
                             visualTransformation = if (passwordVis) VisualTransformation.None else PasswordVisualTransformation(),
                             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
                             value = passConfirmation,
-                            onValueChange = { passConfirmation = it },
+                            onValueChange = {
+                                passConfirmation = it
+                            },
                             label = { Text("Confirm password") }
                         )
                         Button(
                             modifier = Modifier.fillMaxWidth(),
                             onClick = {
->>>>>>> main
-                                viewModel.registerUser(
-                                    email = newUserEmail,
-                                    password = newUserPassText,
-                                    username = newUserId
-                                )
-<<<<<<< HEAD
-                            } else {
-                                passErr = "Passwords do not match."
-                                passConfirmation = ""
-                                newUserPassText = ""
-                            }
-=======
+                                if (newUserPassText.equals(passConfirmation)) {
+                                    viewModel.registerUser(
+                                        email = newUserEmail,
+                                        password = newUserPassText,
+                                        username = newUserId
+                                    )
+                                } else {
+                                    context.dynamicToast("Passwords do not match")
+                                    passConfirmation = ""
+                                    newUserPassText = ""
+                                }
                             }
 
                         ) {
                             Text(text = "Create Account")
->>>>>>> main
                         }
-
                     }
                 }
             }
         }
     }
 }
+
+fun Context.dynamicToast(msg : String) {
+    val toast = Toast.makeText(this, msg, Toast.LENGTH_LONG)
+    toast.setGravity(Gravity.TOP, 0, 0)
+    toast.show()
+}
+
 
